05c08abfdf247711f6d03fbdae0eb5db186efaa4
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-14 08:19:25 +0900

    Finish first

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MaxDigit (int number)
+{
+    int first = number / 10;
+    int second = number % 10;
+    if (first > second) return first;
+    else return second;
+}
+
+int Random ()
+{
+int num = new Random().Next(10,100);
+Console.WriteLine($"Random Number: {num}");
+return num;
+}
+
+
+
+Console.WriteLine ($"Maximal digit is {MaxDigit(Random())}");
