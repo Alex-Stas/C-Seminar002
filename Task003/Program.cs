6a3468d01553b7533c0f63a51303123a16a522f3
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-14 10:55:14 +0900

    Third my

---
+++ Task003/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Принимает на ввод два числа и проверяет является ли первое
+*квадратом второго. Выдает yes/no.
+*/
+Console.Write("Enter first number, please: ");
+string first = Console.ReadLine()!;
+Console.Write("Enter second number, please: ");
+string second = Console.ReadLine()!;
+
+int firstnum = int.Parse(first);
+int secondnum = int.Parse(second);
+string answer = null;
+
+if (firstnum % secondnum == 0)
+{
+    answer = "Yes";
+    // Console.WriteLine("Yes");
+}
+else
+{
+    answer = "No";
+    // Console.WriteLine("No");
+}
+Console.WriteLine (answer);
