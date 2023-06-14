ff704280f66ea648b22de72a0ee98050ef20f77c
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-14 10:42:34 +0900

    Second my

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿int Random()
+{
+    int num = new Random().Next(100, 1000);
+    // Console.WriteLine($"Random Number: {num}");
+    return num;
+}
+
+int SecondFigureDelete(int number)
+{
+    int firstFigure = number / 100;
+    int secondFigure = number % 10;
+    return firstFigure * 10 + secondFigure;
+}
+
+int rnd = Random();
+
+Console.WriteLine($"Random number is: {rnd}; one with deleted second figure is: {SecondFigureDelete(rnd)}");
