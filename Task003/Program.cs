690c19c311140ceb932fe244f838e694e7a578b2
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-14 13:45:31 +0900

    Third my corrected according to TS+

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,23 +1,40 @@
 ﻿/* Принимает на ввод два числа и если первое кратно второму выдает ответ кратно,
 *если нет, то остаток от деления.
 */
-Console.Write("Enter first number, please: ");
-string first = Console.ReadLine()!;
-Console.Write("Enter second number, please: ");
-string second = Console.ReadLine()!;
 
-int firstnum = int.Parse(first);
-int secondnum = int.Parse(second);
+// int DoubleInput()
+// {
+// Console.Write("Enter first number, please: ");
+// string first = Console.ReadLine()!;
+// Console.Write("Enter second number, please: ");
+// string second = Console.ReadLine()!;
+
+// int firstnum = int.Parse(first);
+// int secondnum = int.Parse(second);
+
+
+// return (first, second);
+// }
+
+string Divisible (int fn, int sn)
+{
 string answer = "";
-
-if (firstnum % secondnum == 0)
+if (fn % sn == 0)
 {
     answer = "Divisible";
-    // Console.WriteLine("Yes");
 }
 else
 {
-    answer = $"Non Divisible, reminder is: {firstnum % secondnum}";
-    // Console.WriteLine("No");
+    answer = $"Non Divisible, reminder is: {fn % sn}";
 }
-Console.WriteLine (answer);
+return answer;
+}
+
+Console.Write("Enter first number, please: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number, please: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine (Divisible (first, second));
